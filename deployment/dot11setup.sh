08ab780ca32f0bb2be1466e7df6cd4a4b3315ff6
DepthDeluxe <cheinzmann3@gmail.com>
2015-04-05 19:44:24 -0400

    improved setup bootstrapper script

--- deployment/dot11setup.sh
+++ deployment/dot11setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+registerIPAddress() {
+  hostname=$(hostname)
+  ipaddr=$(ip addr | egrep -o "inet 134\.([0-9]{1,3}[./]){1,3}" | sed -e "s/inet //" -e "s/\///")
+  echo "$hostname $ipaddr" | netcat -c gouda.bucknell.edu 3005
+}
+
 PROOT="/home/sniffer/dot11sniffer"
 
 if [ -d $PROOT ]; then
@@ -12,6 +18,9 @@ if [ -d $PROOT ]; then
   puppet apply "$PROOT/deployment/sniffer.pp"
   echo "Applying puppet file: $?"
 
+  # register IP address with master node
+  registerIPAddress
+
   popd
   exit 0
 fi
@@ -19,7 +28,7 @@ fi
 echo "Project root not found, performing initial setup"
 
 # apply rereq programs
-pacman -S --noconfirm puppet git
+pacman -S --noconfirm puppet git gnu-netcat
 echo "Installing prereq programs: $?"
 
 # create the home directory for the user
@@ -32,5 +41,8 @@ echo "Cloning project dir: $?"
 # apply the puppet file
 puppet apply "$PROOT/deployment/sniffer.pp"
 echo "Applying puppet file: $?"
+
+# register ip address with master node
+registerIPAddress
 
 exit 0
