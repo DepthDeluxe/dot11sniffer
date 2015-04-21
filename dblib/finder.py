b55d865566eb7123b09aee86b8dbd6a9865e8a5f
sdb031 <sdb031@bucknell.edu>
2015-04-21 17:26:55 -0400

    added functions to pull full data from various times and from latest time

--- dblib/finder.py
+++ dblib/finder.py
@@ -1,5 +1,6 @@
 import pymongo
 import datetime
+import time
 
 '''
     time is "day/month/year H:M", nodepos is list with node
@@ -9,15 +10,41 @@ import datetime
 class Finder:
     def __init__(self,nodepos = [(0,0),(100,100),(50,50)]):
         self.nodepos = nodepos
-'''
-    find a mac address based on a time
-'''
-
-    def find(self,mac,time):
+        
+    #Do not use find_mac, it is incomplete
+    '''
+        find a mac address based on a time
+    '''
+    def find_mac(self,mac,time):
         epoch = datetime.datetime.utcfromtimestamp(0)
         current = datetime.datetime.strptime(time,'%d/%m/%Y %H:%M')
-        time = (current - epoch).total_seconds()
+        time = (current - epoch).total_seconds()/600
         
         client = pymongo.MongoClient()
         db = client.cheddar
-        collection = db.times
+        col = db.times
+        data = col.find_one({"_id":time})['data']
+        #doesnt work, we have a list of dictionaries not a dictionary
+        data = {k:v for (k,v) in data.iteritems() if mac in k}
+        return data
+
+    def pull(self):
+        time = time.time()/600
+        
+        client = pymongo.MongoClient()
+        db = client.cheddar
+        col = db.times
+        data = col.find_one({"_id":time})['data']
+        return data
+
+#time is "month/day/year,H:M"
+def pull(self,time):
+        epoch = datetime.datetime.utcfromtimestamp(0)
+        current = datetime.datetime.strptime(time,'%m/%d/%Y,%H:%M')
+        time = ((current - epoch).total_seconds() + 14400)/600 #14400 accounts for eastern timezone
+        
+        client = pymongo.MongoClient()
+        db = client.cheddar
+        col = db.times
+        data = col.find_one({"_id":time})['data']
+        return data
